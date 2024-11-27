0fb932748cb59358a94f74d6d6230ccaca401dba
Luiz Omena <83364189+luizfilipeyep@users.noreply.github.com>
2024-11-27 11:57:17 -0300

    declaração de variáveis e tipos primitivos

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
 public class Main {
   public static void main(String[] args) {
     int idade = 10;
-    string nome = "Luiz";
+    String nome = "Luiz";
 
     var nomeVariavel = "Filipe";
 
-    
+    /*
+    > Números inteiros
+
+    byte -> 8 bits | -128 a 127
+    short -> 16 bits | -32.768 a 32;767
+    int -> 32 bits | -2.147.483.648 a 2.147.483.647
+    long -> 64 bits | -9.223.372.036.854.775.808 e 9.223.372.036.854.775.807
+
+    > Números décimais
+
+    float -> 32 bits | precisão simples
+    double -> 64 bits | precisão dupla
+
+    > Palavras, cadeia e caracteres
+
+    String -> representar palavras e frases ""
+    char -> representar um único caractere ''
+
+    > Lógico
+    boolean -> true ou false
+
+    */
   }
 }
